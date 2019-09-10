98e9441d33533786fa71588270126c8f7a146597
Alberto Cardenas <alberto@hackerops.com>
2019-09-10 16:25:15 -0500

    Update LAMPi.sh

--- LAMPi/LAMPi.sh
+++ LAMPi/LAMPi.sh
@@ -32,13 +32,13 @@ sudo apt install mariadb-server mariadb-client
 sudo mysql_secure_installation
 
 echo -e "$Cian \n Installando PHP & Requirements $resetear_color"
-sudo apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php-imagick php7.0-mcrypt php7.0-mysql php7.0-ps php7.0-xsl -y
+sudo apt-get install sudo apt -qy install php php-common libapache2-mod-php php-curl php-dev php-gd php-gettext php-imagick php-intl php-mbstring php-mysql php-pear php-pspell php-recode php-xml php-zip -y
 
 echo -e "$Cian \n Installando phpMyAdmin $resetear_color"
 sudo apt-get install phpmyadmin -y
 
 echo -e "$Cian \n Verificando instalacion $resetear_color"
-sudo apt-get install apache2 apache2-utils libexpat1 ssl-cert libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php-imagick php7.0-mcrypt php7.0-mysql php7.0-ps php7.0-xsl -y
+sudo apt-get install apache2 apache2-utils libexpat1 ssl-cert sudo apt -qy install php php-common libapache2-mod-php php-curl php-dev php-gd php-gettext php-imagick php-intl php-mbstring php-mysql php-pear php-pspell php-recode php-xml php-zip -y
 
 ## TWEAKS
 # Permisos
