ae2ca5c7758391d0f8d1f53dd2f6b9644eead452
Alberto Cardenas <alberto@hackerops.com>
2019-09-18 10:49:11 -0500

    Update LAMPi.sh

--- LAMPi.sh
+++ LAMPi.sh
@@ -27,14 +27,14 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 echo -e "$Cian \n Installando Apache2 $resetear_color"
 sudo apt-get install apache2 apache2-utils libexpat1 ssl-cert -y
 
-echo -e "$Cian \n Installando Mariadb $resetear_color"
+echo -e "$Cian \n Installando Mariadb (requiere interaccion) $resetear_color"
 sudo apt install mariadb-server mariadb-client
 sudo mysql_secure_installation
 
 echo -e "$Cian \n Installando PHP & Requirements $resetear_color"
 sudo apt-get install php php-common libapache2-mod-php php-curl php-dev php-gd php-gettext php-imagick php-intl php-mbstring php-mysql php-pear php-pspell php-recode php-xml php-zip -y
 
-echo -e "$Cian \n Installando phpMyAdmin $resetear_color"
+echo -e "$Cian \n Installando phpMyAdmin (requiere interaccion) $resetear_color"
 sudo apt-get install phpmyadmin -y
 
 echo -e "$Cian \n Verificando instalacion $resetear_color"
@@ -46,9 +46,9 @@ echo -e "$Cian \n Permisos para /var/www $resetear_color"
 sudo chown -R www-data:www-data /var/www
 echo -e "$Verde \n Los permisos se han establecido $resetear_color"
 
-#Habilitar Mod Rewrite, requerido para enlaces permanentes de WordPress y archivos .htaccess
-#echo -e "$Cian \n habilitando Modulos $resetear_color"
-#sudo a2enmod rewrite
+Habilitar Mod Rewrite, requerido para enlaces permanentes de WordPress y archivos .htaccess
+echo -e "$Cian \n habilitando Modulos $resetear_color"
+sudo a2enmod rewrite
 #sudo php5enmod mcrypt
 
 # Reiniciar Apache
